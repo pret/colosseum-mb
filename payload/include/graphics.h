9b7846c23f26478ca43e9016008d6f131e375066
GriffinR <griffin.g.richards@gmail.com>
2022-01-13 11:27:19 -0500

    Decompile graphics.s

---
+++ payload/include/graphics.h
@@ -0,0 +1,26 @@
+#ifndef GUARD_GRAPHICS_H
+#define GUARD_GRAPHICS_H
+
+extern const u32 gUnknown_02017F0C[];
+extern const u16 gUnknown_0201A450[];
+extern const u32 gUnknown_0201A618[];
+extern const u16 gUnknown_0201A894[];
+extern const u32 gUnknown_0201A8B4[];
+extern const u32 gUnknown_0201AD0C[];
+extern const u32 gUnknown_0201B0FC[];
+extern const u16 gUnknown_0201B1A0[];
+extern const u32 gUnknown_0201B2F8[];
+extern const u32 gUnknown_0201B430[];
+extern const u32 gUnknown_0201B4AC[];
+extern const u32 gUnknown_0201BFF8[];
+extern const u16 gUnknown_0201C10C[];
+extern const u32 gUnknown_0201C228[];
+extern const u32 gUnknown_0201C428[];
+extern const u32 gUnknown_0201C5A4[];
+extern const u16 gUnknown_0201D05C[];
+extern const u32 gUnknown_0201D180[];
+extern const u16 gUnknown_0201D3F0[];
+extern const u32 gUnknown_0201D420[];
+extern const u32 gUnknown_0201D5A4[];
+
+#endif // GUARD_GRAPHICS_H
